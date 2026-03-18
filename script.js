38bbd0e8ee31cc3f37c4b655a96879ede0c7f0c8
annaferrerherrera <annaferrerherrera@gmail.com>
2026-03-18 13:24:02 +0100

    script.js

---
+++ script.js
@@ -0,0 +1,30 @@
+const images = [
+  "img1.jpg",
+  "img2.jpg",
+  "img3.jpg",
+  "img4.jpg"
+];
+
+let index = 0;
+
+document.addEventListener("mousemove", (e) => {
+  const img = document.createElement("img");
+
+  img.src = images[index];
+  img.style.position = "fixed";
+  img.style.left = e.clientX + "px";
+  img.style.top = e.clientY + "px";
+  img.style.width = "120px";
+  img.style.transform = "translate(-50%, -50%)";
+  img.style.pointerEvents = "none";
+  img.style.opacity = 0.8;
+
+  document.body.appendChild(img);
+
+  // eliminar después de un tiempo
+  setTimeout(() => {
+    img.remove();
+  }, 500);
+
+  index = (index + 1) % images.length;
+});
